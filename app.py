10494610f8268bd30dfed1e56b6b4dc4c0db96f9
Alex Libman <kingalex11235@yahoo.com>
2014-10-18 23:40:17 -0400

    first database implements and links to blogs created

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # Database Project
 
 from flask import Flask, render_template, request
+import sqlite3
 #import utils
 
 # app is an instance of the Flask class
@@ -11,8 +12,12 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 @app.route("/<title>")
 def home(title=None):
-	if title==None:
-		return render_template("index.html")
+        conn = sqlite3.connect("blogs.db")
+        c = conn.cursor()
+        if title==None:
+                q = "SELECT title FROM blogs"
+                result = c.execute(q)
+                return render_template("index.html",titles=result)
 
 if __name__=="__main__":
     # set the instance variable debug to True
---
+++ populate.py
@@ -0,0 +1,25 @@
+import csv, sqlite3
+
+#run populate.py only if blogs.db is not present
+conn = sqlite3.connect("blogs.db")
+c = conn.cursor()
+
+c.execute("CREATE TABLE blogs (title TEXT, entry TEXT, id INTEGER UNIQUE)")
+
+base = 'INSERT INTO blogs VALUES("%(title)s","%(entry)s",%(id)s)'
+for a in csv.DictReader(open("blogposts.csv")):
+    i = base%a
+    print i
+    c.execute(i)
+
+conn.commit()
+
+c.execute("CREATE TABLE comments (title TEXT, entry TEXT, id INTEGER)")
+
+base = 'INSERT INTO comments VALUES("%(name)s","%(comment)s",%(id)s)'
+for a in csv.DictReader(open("comments.csv")):
+    i = base%a
+    print i
+    c.execute(i)
+
+conn.commit()
