2c87ab449fb8cd6e6621be9839a0e381fafb71d4
Leslie Bresnahan <les.bres.2@gmail.com>
2014-10-20 12:53:04 -0400

    Update populate.py

--- populate.py
+++ populate.py
@@ -4,9 +4,9 @@ import csv, sqlite3
 conn = sqlite3.connect("blogs.db")
 c = conn.cursor()
 
-c.execute("CREATE TABLE blogs (title TEXT, entry TEXT, id INTEGER UNIQUE)")
+c.execute("CREATE TABLE blogs (title TEXT, name TEXT, entry TEXT, id INTEGER UNIQUE)")
 
-base = 'INSERT INTO blogs VALUES("%(title)s","%(entry)s",%(id)s)'
+base = 'INSERT INTO blogs VALUES("%(title)s","%(name)s", "%(entry)s",%(id)s)'
 for a in csv.DictReader(open("blogposts.csv")):
     i = base%a
     print i
@@ -14,7 +14,7 @@ for a in csv.DictReader(open("blogposts.csv")):
 
 conn.commit()
 
-c.execute("CREATE TABLE comments (title TEXT, entry TEXT, id INTEGER)")
+c.execute("CREATE TABLE comments (name TEXT, comment TEXT, id INTEGER)")
 
 base = 'INSERT INTO comments VALUES("%(name)s","%(comment)s",%(id)s)'
 for a in csv.DictReader(open("comments.csv")):
