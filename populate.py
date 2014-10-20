a3b2647e9a95f8a92e5c160dd76277c1d2c3a255
Leslie Bresnahan <les.bres.2@gmail.com>
2014-10-20 13:05:17 -0400

    Update populate.py

--- populate.py
+++ populate.py
@@ -1,25 +1,39 @@
 import csv, sqlite3
 
-#run populate.py only if blogs.db is not present
-conn = sqlite3.connect("blogs.db")
-c = conn.cursor()
+def setup():
+    #run populate.py only if blogs.db is not present
+    conn = sqlite3.connect("blogs.db")
+    c = conn.cursor()
 
-c.execute("CREATE TABLE blogs (title TEXT, name TEXT, entry TEXT, id INTEGER UNIQUE)")
+    c.execute("CREATE TABLE blogs (title TEXT, name TEXT, entry TEXT, id INTEGER UNIQUE)")
+    
+    conn.commit()
 
-base = 'INSERT INTO blogs VALUES("%(title)s","%(name)s", "%(entry)s",%(id)s)'
-for a in csv.DictReader(open("blogposts.csv")):
-    i = base%a
-    print i
-    c.execute(i)
+    c.execute("CREATE TABLE comments (name TEXT, comment TEXT, id INTEGER)")
+    
+    base = 'INSERT INTO blogs VALUES("%(title)s","%(name)s", "%(entry)s",%(id)s)'
+    for a in csv.DictReader(open("blogposts.csv")):
+        i = base%a
+        print i
+        c.execute(i)
+        
+    base = 'INSERT INTO comments VALUES("%(name)s","%(comment)s",%(id)s)'
+    for a in csv.DictReader(open("comments.csv")):
+        i = base%a
+        print i
+        c.execute(i)
+    
+    conn.commit()
+    
 
-conn.commit()
+def insert_post(title, name, entry, _id):
 
-c.execute("CREATE TABLE comments (name TEXT, comment TEXT, id INTEGER)")
+    c.execute( '''INSERT INTO blogs VALUES(''' + title + ''', ''' + name + ''', ''' + entry + ''', ''' + _id + ''')''' )
+    conn.commit()    
+    
 
-base = 'INSERT INTO comments VALUES("%(name)s","%(comment)s",%(id)s)'
-for a in csv.DictReader(open("comments.csv")):
-    i = base%a
-    print i
-    c.execute(i)
+def insert_comment(name, comment, _id):
+    
+    c.execute( '''INSERT INTO comments VALUES(''' + name + ''', ''' + comment + ''', ''' + _id + ''')''')
+    conn.commit()
 
-conn.commit()
