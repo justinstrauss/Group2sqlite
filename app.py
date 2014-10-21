6d21cf0a7b016a959a2ee955208a0ed3be8f1431
alb-yeung <alb.yeung@gmail.com>
2014-10-20 20:34:46 -0400

    made stuff clickable

--- app.py
+++ app.py
@@ -21,14 +21,28 @@ def home(title=None):
         if title==None:
                 q = "SELECT title FROM blogs"
                 result = c.execute(q)
-                return render_template("index.html",titles=result)
+                return render_template("index.html",titles=result,range = range(result.len()))
         else:
                 t = title.replace("_"," ")
                 q = '''SELECT title,entry,id FROM blogs
                        WHERE title = ''' + t
                 result = c.execute(q)
                 return render_template("blog.html",blogs=result)
+
+
                 
+@app.route("/post")
+@app.route("/1")
+@app.route("/2")
+def post():
+    f = open("blogposts.csv")
+    data = f.split("\n")
+    f.close()
+    for n in data:
+        number = 1
+        return render_template("post.html",text = data[number])
+
+
 if __name__=="__main__":
     # set the instance variable debug to True
     app.debug = True
